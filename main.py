08467682a6cb24eb538c1b97482384ee1bbaa93e
ivan-forcen <ivan.lim@forcen.tech>
2022-11-03 11:08:39 -0400

    new helper functions for mcp

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from mcp_functions import Mcp
 
 if __name__ == "__main__":
     dev=Ftdi()
-    dev.SPI_Initial()
+    #dev.SPI_Initial()
+    dev.set_bitmode(0x00,0x02)
+    dev.set_spi_clock(1000000)
     dev.SPI_CSDisable('a')
     dev.info()
     dev.loopback()
-    time.sleep(2)
     dev.SPI_CSEnable('a')
-    dev.write_cmd_bytes(0x31,(0x14,0x8B))#0xA9,0x5D
+    dev.write_cmd_bytes(0x31,(0x90,0xF3))#0xA9,0x5D
     print(dev.dread(2))
     dev.dwrite((0xAA,0x00))
     print(dev.dread(4))
--- mcp_functions/functions.py
+++ mcp_functions/functions.py
@@ -1,42 +1,56 @@
 from ftd_functions import Ftdi
-
 class Mcp:
     def __init__(self,dev):
+        dev=Ftdi()
         self.d=dev
     #resets the mcp chip
     def reset(self):
-        self.SPI_CSEnable()
+        self.d.SPI_CSEnable('a')
         self.d.write_cmd_bytes(0x10,(0b0000,0x000))
-        self.SPI_CSDisable()
+        self.d.SPI_CSDisable('a')
     #reads data from specific address
     def read(self,a,length):
-        self.SPI_CSEnable()
-        self.d.write_cmd_bytes(0x10,(0b0011,a))
-        self.d.write_cmd_bytes(0x21,(length))
-        self.d.read(length)
-        self.SPI_CSDisable()
+        self.d.SPI_CSEnable('a')
+        self.d.write_cmd_bytes(0x11,(0b0011,a))
+        self.d.write_cmd_bytes(0x20,(length))
+        state = self.d.dread(length)
+        self.d.SPI_CSDisable('a')
+        return state
     #writes data to specific address
     def write(self,a,data):
-        self.SPI_CSEnable()
-        self.d.write_cmd_bytes(0x10,(0b0010,a,data))
-        self.SPI_CSDisable()
+        self.d.SPI_CSEnable('a')
+        self.d.write_cmd_bytes(0x11,(0b0010,a,data))
+        self.d.SPI_CSDisable('a')
     #reads the cyclic redundancy test
     def read_crc(self,a,n):
-        self.SPI_CSEnable()
+        self.d.SPI_CSEnable('a')
         self.d.write_cmd_bytes(0x10,(0b1011,a))
-        self.d.read(1)
-        self.SPI_CSDisable()
+        self.d.dread(1)
+        self.d.SPI_CSDisable('a')
     #writes crc 2 bytes, calculated from Address/N data bytes/Data
     def write_crc(self,a,data,crc):
-        self.SPI_CSEnable()
+        self.d.SPI_CSEnable('a')
         self.d.write_cmd_bytes(0x10,(0b1010,a,data,crc))
-        self.SPI_CSDisable()
+        self.d.SPI_CSDisable('a')
     #write sfr/ram to address A
     def write_safe(self,a,data,crc):
-        self.SPI_CSEnable()
+        self.d.SPI_CSEnable('a')
         self.d.write_cmd_bytes(0x10,(0b1100,a,data,crc))
-        self.SPI_CSDisable()
+        self.d.SPI_CSDisable('a')
 
-    """def getQueueStatus()"""
+    def clearTx(self):
+        self.d.SPI_CSEnable('a')
+        self.write(0x033,1)
+        self.d.SPI_CSDisable('a')
+    
+    def getOpMode(self):
+        self.d.SPI_CSEnable('a')
+        state = self.read(0x03,1)
+        return state[5:7]
+
+    def setOpMode(self,mode):#only possible in configuration mode, mode int 0 to 7
+        self.d.SPI_CSEnable('a')
+        self.write(0x030,mode)
+        self.d.SPI_CSDisable('a')
     
     
