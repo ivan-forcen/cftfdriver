888f2f3c96b7916b570a12faaf0af9bd9bacc7bb
ivan-forcen <ivan.lim@forcen.tech>
2022-11-03 10:10:24 -0400

    osci fix

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from mcp_functions import Mcp
 if __name__ == "__main__":
     dev=Ftdi()
     dev.SPI_Initial()
-    dev.SPI_CSDisable
+    dev.SPI_CSDisable('a')
     dev.info()
     dev.loopback()
-    dev.SPI_CSEnable
-    dev.write_cmd_bytes(0x31,(169,93))#0b10101001,0b01011101
+    time.sleep(2)
+    dev.SPI_CSEnable('a')
+    dev.write_cmd_bytes(0x31,(0x14,0x8B))#0xA9,0x5D
     print(dev.dread(2))
     dev.dwrite((0xAA,0x00))
     print(dev.dread(4))
